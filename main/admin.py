addd784c7fcd148cc48a6524ebd150ef8159f559
Sweet As KaraKum <76500727+SwAsKk@users.noreply.github.com>
2022-04-30 21:10:40 +0300

    Модели и админка

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ClassicSet, BakedSet, ComboSet
+
+
 
 # Register your models here.
+admin.site.register(ClassicSet)
+admin.site.register(BakedSet)
+admin.site.register(ComboSet)
--- main/models.py
+++ main/models.py
@@ -115,3 +115,155 @@ class BakedSet(models.Model):
         verbose_name = 'Запечённый сет'
         verbose_name_plural = 'Запечённые сеты'
 
+
+class Sushi(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length= 30,
+        null= False,
+        blank = False,
+    )
+
+    price = models.IntegerField(
+        verbose_name= 'Цена',
+        null= False,
+        blank = False,
+    )
+
+    compound = models.CharField(
+        verbose_name= 'Состав',
+        null = False,
+        blank = False,
+        max_length= 200,
+    )
+
+    weight = models.IntegerField(
+        verbose_name= 'Вес',
+        null= False,
+        blank = False,
+    )
+
+    def get_absolute_url(self):
+        return reverse('sushi_details_url', kwargs = {'id': self.id})
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name, self.price, self.compound, self.weight)
+
+    class Meta:
+        verbose_name = 'Суши'
+        verbose_name_plural = 'Суши'
+
+
+class Rolls(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length= 30,
+        null= False,
+        blank = False,
+    )
+
+    price = models.IntegerField(
+        verbose_name= 'Цена',
+        null= False,
+        blank = False,
+    )
+
+    compound = models.CharField(
+        verbose_name= 'Состав',
+        null = False,
+        blank = False,
+        max_length= 200,
+    )
+
+    weight = models.IntegerField(
+        verbose_name= 'Вес',
+        null= False,
+        blank = False,
+    )
+
+    def get_absolute_url(self):
+        return reverse('rolls_details_url', kwargs = {'id': self.id})
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name, self.price, self.compound, self.weight)
+
+    class Meta:
+        verbose_name = 'Роллы'
+        verbose_name_plural = 'Роллы'
+
+
+class Wok(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length= 30,
+        null= False,
+        blank = False,
+    )
+
+    price = models.IntegerField(
+        verbose_name= 'Цена',
+        null= False,
+        blank = False,
+    )
+
+    compound = models.CharField(
+        verbose_name= 'Состав',
+        null = False,
+        blank = False,
+        max_length= 200,
+    )
+
+    weight = models.IntegerField(
+        verbose_name= 'Вес',
+        null= False,
+        blank = False,
+    )
+
+    def get_absolute_url(self):
+        return reverse('wok_details_url', kwargs = {'id': self.id})
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name, self.price, self.compound, self.weight)
+
+    class Meta:
+        verbose_name = 'Вок'
+        verbose_name_plural = 'Вок'
+
+
+class Pizza(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length= 30,
+        null= False,
+        blank = False,
+    )
+
+    price = models.IntegerField(
+        verbose_name= 'Цена',
+        null= False,
+        blank = False,
+    )
+
+    compound = models.CharField(
+        verbose_name= 'Состав',
+        null = False,
+        blank = False,
+        max_length= 200,
+    )
+
+    weight = models.IntegerField(
+        verbose_name= 'Вес',
+        null= False,
+        blank = False,
+    )
+
+    def get_absolute_url(self):
+        return reverse('pizza_details_url', kwargs = {'id': self.id})
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name, self.price, self.compound, self.weight)
+
+    class Meta:
+        verbose_name = 'Пицца'
+        verbose_name_plural = 'Пицца'
+
