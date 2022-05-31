4072f046d707747487bf0fec51c0d474d637f0f8
ITRONgroup <76500727+SwAsKk@users.noreply.github.com>
2022-05-31 21:45:03 +0300

    снес всё нахер

--- main/admin.py
+++ main/admin.py
@@ -4,21 +4,4 @@ from .models import *
 
 
 # Register your models here.
-admin.site.register(ClassicSet)
-admin.site.register(BakedSet)
-admin.site.register(ComboSet)
-admin.site.register(Sushi)
-admin.site.register(Rolls)
-admin.site.register(Wok)
-admin.site.register(Pizza)
-admin.site.register(BakedSets)
-admin.site.register(Sets)
-admin.site.register(Tempured)
-admin.site.register(Soup)
-admin.site.register(HotAndSalads)
-admin.site.register(MiniRolls)
-admin.site.register(MagRolls)
-admin.site.register(Vegan)
-admin.site.register(Deserts)
-admin.site.register(Drinks)
-admin.site.register(SushiStuff)
+admin.site.register(Product)
--- main/views.py
+++ main/views.py
@@ -22,94 +22,94 @@ def index(request):
 
 def baked(request):
     c = base_context(request)
-    ob = BakedSet.objects.all()
+    ob = Product.objects.filter(type="Запечённые роллы")
     c['baked_list'] = ob
     return render(request, 'baked.html', c)
 
 
 def sushi(request):
     c = base_context(request)
-    ob = Sushi.objects.all()
+    ob = Product.objects.filter(type="Суши")
     c['sushi_list'] = ob
     return render(request, 'sushi.html', c)
 
 def rolls(request):
     c = base_context(request)
-    ob = Rolls.objects.all()
+    ob = Product.objects.filter(type="Роллы")
     c['rolls_list'] = ob
     return render(request, 'rolls.html', c)
 
 def sets(request):
     c = base_context(request)
-    ob = Sets.objects.all()
+    ob = Product.objects.filter(type="Наборы")
     c['sets_list'] = ob
     return render(request, 'sets.html', c)
 
 def tempured(request):
     c = base_context(request)
-    ob = Tempured.objects.all()
+    ob = Product.objects.filter(type="Темпура")
     c['tempured_list'] = ob
     return render(request, 'tempured.html', c)
 
 
 def wok(request):
     c = base_context(request)
-    ob = Wok.objects.all()
+    ob = Product.objects.filter(type="Лапша wok")
     c['wok_list'] = ob
     return render(request, 'wok.html', c)
 
 def soup(request):
     c = base_context(request)
-    ob = Soup.objects.all()
+    ob = Product.objects.filter(type="Супы")
     c['soup_list'] = ob
     return render(request, 'soup.html', c)
 
 
 def hotandsalads(request):
     c = base_context(request)
-    ob = HotAndSalads.objects.all()
+    ob = Product.objects.filter(type="Горячее и салаты")
     c['hotandsalads_list'] = ob
     return render(request, 'hotandsalads.html', c)
 
 def minirolls(request):
     c = base_context(request)
-    ob = MiniRolls.objects.all()
+    ob = Product.objects.filter(type="Мини роллы")
     c['minirolls_list'] = ob
     return render(request, 'minirolls.html', c)
 
 def magrolls(request):
     c = base_context(request)
-    ob = MagRolls.objects.all()
+    ob = Product.objects.filter(type="Маг роллы")
     c['magrolls_list'] = ob
     return render(request, 'magrolls.html', c)
 
 def vegan(request):
     c = base_context(request)
-    ob = Vegan.objects.all()
+    ob = Product.objects.filter(type="Вегетарианское меню")
     c['vegan_list'] = ob
     return render(request, 'vegan.html', c)
 
 def deserts(request):
     c = base_context(request)
-    ob = Deserts.objects.all()
+    ob = Product.objects.filter(type="Десерты")
     c['deserts_list'] = ob
     return render(request, 'deserts.html', c)
 
 def drinks(request):
     c = base_context(request)
-    ob = Drinks.objects.all()
+    ob = Product.objects.filter(type="Напитки")
     c['drinks_list'] = ob
     return render(request, 'drinks.html', c)
 
 def stuff(request):
     c = base_context(request)
-    ob = SushiStuff.objects.all()
+    ob = Product.objects.filter(type="Всё для суши")
     c['stuff_list'] = ob
     return render(request, 'stuff.html', c)
 
 def pizza(request):
     c = base_context(request)
-    ob = Pizza.objects.all()
+    ob = Product.objects.filter(type="Пицца")
     c['pizza_list'] = ob
     return render(request, 'pizza.html', c)
 
