661fc3611ccaf1687bad6b24264145bee2bb4501
Sweet As KaraKum <lev.sobolev.03@mail.ru>
2022-12-05 14:03:36 +0300

    Fix of views.py

--- cart/views.py
+++ cart/views.py
@@ -11,6 +11,8 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 
+
+
 def base_context(request):
     context = dict()
     context['user'] = request.user
@@ -66,12 +68,12 @@ def cart_detail(request):
             }
             if form.cleaned_data['city'] == 'spb':
                 html_body = render_to_string("mail.html", data)
-                msg = EmailMultiAlternatives (subject="Новый заказ!", from_email='sushiman.web@mail.ru', to=['sushiman.spb@mail.ru',])
+                msg = EmailMultiAlternatives (subject="Новый заказ!", from_email='from_mail@gmail.com', to=['to_mail@gmail.com',])
                 msg.attach_alternative(html_body, "text/html")
                 msg.send()
             elif form.cleaned_data['city'] == 'bal':
                 html_body = render_to_string("mail.html", data)
-                msg = EmailMultiAlternatives (subject="Новый заказ!", from_email='sushiman.web@mail.ru', to=['sushiman.msk@mail.ru',])
+                msg = EmailMultiAlternatives (subject="Новый заказ!", from_email='from_mail@gmail.com', to=['to_mail@gmail.com',])
                 msg.attach_alternative(html_body, "text/html")
                 msg.send()
             return redirect('/')
